4d8e2ed1d3e9c7e641bd03ae4e7e6d6027a17b2c
ST10217550 <st10217550@rcconnect.edu.za>
2024-08-15 18:39:22 +0000

    second commit

--- app/src/main/java/com/example/wilproject/HomePage.java
+++ app/src/main/java/com/example/wilproject/HomePage.java
@@ -1,8 +1,11 @@
 package com.example.wilproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +13,41 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        CardView profile = findViewById(R.id.profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, ProfilePage.class);
+                startActivity(intent);
+            }
+        });
+
+        CardView getDate = findViewById(R.id.getDate);
+        getDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, GetDatePage.class);
+                startActivity(intent);
+            }
+        });
+
+        CardView reschedule = findViewById(R.id.reschedule);
+        reschedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, ReschedulePage.class);
+                startActivity(intent);
+            }
+        });
+
+        CardView track = findViewById(R.id.track);
+        track.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, TrackDatesPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
