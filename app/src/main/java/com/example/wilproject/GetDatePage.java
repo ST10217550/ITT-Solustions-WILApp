5f1e37cb48bc41097cda2ca33c1d50e768092927
ST10217550 <st10217550@rcconnect.edu.za>
2024-08-22 12:12:55 +0000

    update commit

--- app/src/main/java/com/example/wilproject/GetDatePage.java
+++ app/src/main/java/com/example/wilproject/GetDatePage.java
@@ -2,13 +2,20 @@ package com.example.wilproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class GetDatePage extends AppCompatActivity {
 
+    Spinner illnesses;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_date_page);
+
+        illnesses = findViewById(R.id.illnesses);
     }
 }
